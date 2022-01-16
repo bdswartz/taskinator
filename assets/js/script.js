977a9131631f98328f74912c897a4bccd71eaa86
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-01-16 11:21:21 -0500

    completed the edit task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,36 @@ var taskFormHandler = function(event) {
         return false;
       };
     formEl.reset();
-    // Create Object
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
-      };
+    
+    var isEdit = formEl.hasAttribute("data-task-id");
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+
+    else {
+        // Create Object
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+          };
     //   Call Create Task Function and send object as argument
     createTaskEl(taskDataObj);
 };
+};
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    // find the matching task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    // set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+
+  };
 
 var createTaskEl = function(taskDataObj) {
     // Create list item
